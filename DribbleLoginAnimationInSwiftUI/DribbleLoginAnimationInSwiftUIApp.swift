57481e8b4db7dc3c56706706656503b67dd35630
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-26 12:00:23 +0200

    Initial Commit

---
+++ DribbleLoginAnimationInSwiftUI/DribbleLoginAnimationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DribbleLoginAnimationInSwiftUIApp.swift
+//  DribbleLoginAnimationInSwiftUI
+//
+//  Created by ramil on 26.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct DribbleLoginAnimationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
